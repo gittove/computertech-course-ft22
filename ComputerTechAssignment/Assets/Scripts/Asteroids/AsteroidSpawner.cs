4dc512d20b81fd0081e8fcc3d01245d828bc8938
Tove Backenroth <tovebac@hotmail.com>
2022-12-09 15:01:36 +0100

    asteroid prefab, spawner follow player

--- ComputerTechAssignment/Assets/Scripts/Asteroids/AsteroidSpawner.cs
+++ ComputerTechAssignment/Assets/Scripts/Asteroids/AsteroidSpawner.cs
@@ -5,32 +5,46 @@ using UnityEngine;
 public class AsteroidSpawner : MonoBehaviour
 {
     [Header("Offset (metres) in front of player, originating from the player's position")]
-    [SerializeField] private int zOffset = 30;
-    [SerializeField] private int yOffset = 15;
+    [SerializeField] private int forwardOffset = 30;
+    [SerializeField] private int upOffset = 15;
 
     [Header("Radius (metres) from centre of object in all axis directions")]
     [SerializeField] private int spawnRadiusMetres = 15;
     [SerializeField] private int SpawnIntervalInSeconds = 2;
     [SerializeField] private GameObject[] asteroidPrefabs;
+    [SerializeField] private GameObject player;
 
     private float spawnTimer = 0.0f;
     private Vector3 spawnPosition;
     private Bounds SpawnArea;
     private Transform transform;
-
+    private Transform playerTransform;
     void Start()
     {
         transform = GetComponent<Transform>();
+        playerTransform = player.GetComponent<Transform>();
     }
 
     void Update()
     {
+        FollowPlayer();
+
         spawnTimer += Time.deltaTime;
 
         if (spawnTimer >= SpawnIntervalInSeconds)
         {
             SpawnAsteroid();
         }
+    }
+    void FollowPlayer()
+    {
+        Vector3 newPosition = Vector3.zero;
+
+        newPosition = playerTransform.position;
+        newPosition += playerTransform.forward * this.forwardOffset;
+        newPosition.y += upOffset;
+
+        transform.position = newPosition;
     }
 
     void SpawnAsteroid()
